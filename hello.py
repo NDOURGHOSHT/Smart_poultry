257a82213f5a6a74f5e1a2387ffa9297a411c412
ndour_the_boss <capolemasterchef1999@gmail.com>
2026-03-06 16:21:26 +0000

    phase test

---
+++ hello.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+
+def index():
+    first_name ="ndour"
+    stuff =" This is Bold text "
+    favorite_pizza = ["mango","limon","khall",41]
+    return render_template("index.html",
+                           first_name=first_name,
+                           stuff=stuff,
+                           favorite_pizza = favorite_pizza)
+
+
+@app.route('/user/<name>')
+def user(name):
+    return render_template("user.html",user_name=name)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
+
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template("500.html"), 500
+
+if __name__ == "__main__":
+    app.run(debug=True)
