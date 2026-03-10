7c1dc5330f2f6bbbd3ff95dfe914c7a2395e6fec
ndour_the_boss <capolemasterchef1999@gmail.com>
2026-03-10 00:55:07 +0000

    Flask avec WTForm

--- hello.py
+++ hello.py
@@ -1,12 +1,22 @@
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "le mot de passe que personne ne doit savoir "
 
+#Creation de forme
+
+class NamerForm(FlaskForm):
+    name = StringField("Quel est ton nom :", validators=[DataRequired()])
+    submit = SubmitField("submit")
+ 
 @app.route('/')
 
 def index():
-    first_name ="ndour"
-    stuff =" This is Bold text "
+    first_name = "ndour"
+    stuff = " This is Bold text "
     favorite_pizza = ["mango","limon","khall",41]
     return render_template("index.html",
                            first_name=first_name,
@@ -26,5 +36,23 @@ def page_not_found(e):
 def page_not_found(e):
     return render_template("500.html"), 500
 
+@app.route('/name', methods=['GET', 'POST'])
+def name():
+    name = None
+    form = NamerForm()
+    #validation de la forme
+    if form.validate_on_submit():
+        name= form.name.data
+        form.name.data = ''
+
+    return render_template("name.html",
+                           name = name,
+                           form = form)
+
 if __name__ == "__main__":
     app.run(debug=True) 
+
+
+
+
+
