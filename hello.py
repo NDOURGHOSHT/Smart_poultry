d0c4b6a8faae5d48e002c3fb9df576ae309c2de3
ndour_the_boss <capolemasterchef1999@gmail.com>
2026-03-17 23:31:32 +0000

    J'ai ajouter le add_user pour permettre l'enregistrement

--- hello.py
+++ hello.py
@@ -2,18 +2,58 @@ from flask import Flask, render_template, flash
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
+from flask_sqlalchemy import SQLAlchemy 
+from datetime import datetime
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "le mot de passe que personne ne doit savoir "
+# ajout d'un database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+#Initialisation du database
+db = SQLAlchemy(app)
+
+#Creation de modele
+class Users (db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200),nullable=False)
+    email = db.Column(db.String(120),nullable=False, unique=True)
+    date_added = db.Column(db.DateTime, default=datetime.now)
+    
+    #Cree un String
+    def __repr__(self):
+        return '<name %r>' % self.name
 
 #Creation de forme
+
+class UserForm(FlaskForm):
+    name = StringField("Name:", validators=[DataRequired()])
+    email = StringField("Email:", validators=[DataRequired()])
+    submit = SubmitField("submit")
 
 class NamerForm(FlaskForm):
     name = StringField("Quel est ton nom :", validators=[DataRequired()])
     submit = SubmitField("submit")
  
-@app.route('/')
 
+
+@app.route('/user/add', methods=['GET','POST'])
+def add_user():
+    name = None
+    form = UserForm()
+    if form.validate_on_submit():
+       user = Users.query.filter_by(email=form.email.data).first()
+       if user is None:
+        user = Users(name=form.name.data, email=form.email.data)
+        db.session.add(user) 
+        db.session.commit()
+        name = form.name.data
+        form.name.data= ''
+        form.email.data= ''
+        flash("Utilisateur ajouter avec succes!!!")
+    our_users= Users.query.order_by(Users.date_added)
+    return render_template("add_user.html", form=form, name=name,
+                           our_users=our_users )
+@app.route('/')
 def index():
     first_name = "ndour"
     stuff = " This is Bold text "
@@ -45,6 +85,8 @@ def name():
         name= form.name.data
         form.name.data = ''
         flash("Bienvenue dans notre page web !!!")
+
+
 
     return render_template("name.html",
                            name = name,
